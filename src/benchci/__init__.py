21e570d4d48a8651f1ade41c357d82585174bac8
Chris <363708+christopherwoodall@users.noreply.github.com>
2025-09-08 15:10:15 -0500

    Update __init__.py

--- src/benchci/__init__.py
+++ src/benchci/__init__.py
@@ -1,9 +1,20 @@
-# ruff: noqa: F401
+# ruff: noqa
 # Configure clean imports for the package
 # See: https://hynek.me/articles/testing-packaging/
 
 from . import pages, charts, evaluation
 from .evaluation import load_config, run_evaluation
+from .pages import build_pages
+from .charts import build_charts
 
+__all__ = [
+    "evaluation",
+        "run_evaluation",
+        "load_config",
 
-__all__ = ["evaluation", "run_evaluation", "load_config", "pages", "charts"]
+    "pages",
+        "build_pages",
+
+    "charts",
+        "build_charts",
+]
