0fdac968976b85b3726dfe3d45dbc77d67af647e
Chris <363708+christopherwoodall@users.noreply.github.com>
2025-09-08 14:28:04 -0500

    Update imports

--- src/benchci/__init__.py
+++ src/benchci/__init__.py
@@ -2,7 +2,7 @@
 # Configure clean imports for the package
 # See: https://hynek.me/articles/testing-packaging/
 
-from . import evaluation
+from . import evaluation, pages, charts
 from .evaluation import run_evaluation, load_config
 
-__all__ = ["evaluation", "run_evaluation", "load_config"]
+__all__ = ["evaluation", "run_evaluation", "load_config", "pages", "charts"]
--- src/benchci/entrypoint.py
+++ src/benchci/entrypoint.py
@@ -19,18 +19,23 @@ def main():
     parser = argparse.ArgumentParser(description="BenchCI Command Line Interface")
     subparsers = parser.add_subparsers(dest="command")
 
-    # TODO: Pass config file to each command
     eval_parser = subparsers.add_parser("evaluate", help="Run evaluations")
     eval_parser.set_defaults(func=run_evaluation)
+    eval_parser.add_argument("--config", type=str, default="config.yaml", help="Path to the YAML configuration file")
 
     page_parser = subparsers.add_parser("build-pages", help="Build HTML pages")
     page_parser.set_defaults(func=build_pages)
+    page_parser.add_argument("--config", type=str, default="config.yaml", help="Path to the YAML configuration file")
 
     chart_parser = subparsers.add_parser("build-charts", help="Build charts")
     chart_parser.set_defaults(func=build_charts)
-    
+    chart_parser.add_argument("--config", type=str, default="config.yaml", help="Path to the YAML configuration file")
+
     args = parser.parse_args()
 
+    config_file_path = args.config if hasattr(args, "config") else "config.yaml"
+    config = benchci.evaluation.load_config(config_file_path)
+    
     if hasattr(args, "func"):
         args.func()
     else:
