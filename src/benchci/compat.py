69c27570cd1024ecc056bf6e4cfdbeda2ef04c5b
Chris <363708+christopherwoodall@users.noreply.github.com>
2025-09-15 13:37:52 -0500

    update suffixes

--- src/benchci/compat.py
+++ src/benchci/compat.py
@@ -37,18 +37,16 @@ def iter_json_files(logs_path="./logs/"):
 def compat_logs():
     # TODO - Better define output paths (config.yaml?) - evaluation.py:28
     for record in iter_json_files("./logs/"):
+        # If the file is already compat, skip it
+        if "_compat" in record.name:
+            continue
+
         try:
             with record.open("r", encoding="utf-8") as f:
                 record_json = json.load(f)
         except json.JSONDecodeError:
             print(f"Warning: Could not decode JSON from {record}")
             continue
-
-        # If the file is already compat, skip it
-        if "_compat" in record.name:
-            continue
-        
-        # TODO - Check if error exists in log and remove it?
 
         # Remove evaluation content from the JSON
         record_json.pop("samples", None)
@@ -58,8 +56,12 @@ def compat_logs():
         with record.open("w", encoding="utf-8") as f:
             json.dump(record_json, f, indent=4, sort_keys=True, ensure_ascii=False)
 
+        # Strip all "_compats" first to avoid multiple suffixes
+        compat_stem = record.stem.replace("_compat", "")
         # Rename the file to prepend "_compat"
-        compat_rename = record.with_name(f"{record.stem}_compat{record.suffix}")
+        compat_rename = record.with_name(f"{compat_stem}_compat{record.suffix}")
         record.rename(compat_rename)
 
         print(f"Processed {record} and saved compacted version as {compat_rename}")
+
+    print("Log compaction complete.")
