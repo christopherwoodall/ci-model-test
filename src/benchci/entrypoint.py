d579a93753d480450ca01017d5f5fdd90cb56b6f
Chris <363708+christopherwoodall@users.noreply.github.com>
2025-09-17 14:27:23 -0500

    Update entrypoint.py

--- src/benchci/entrypoint.py
+++ src/benchci/entrypoint.py
@@ -2,22 +2,6 @@ import benchci
 import argparse
 
 
-def run_evaluation(config_file_path):
-    benchci.evaluation.run_evaluation(config_file_path)
-
-
-def build_pages(config_file_path):
-    benchci.pages.build_pages(config_file_path)
-
-
-def build_charts(config_file_path):
-    benchci.charts.build_charts(config_file_path)
-
-
-def compat_logs(config_file_path):
-    benchci.compat.compat_logs(config_file_path)
-
-
 def main():
     parser = argparse.ArgumentParser(description="BenchCI Command Line Interface")
 
@@ -31,16 +15,16 @@ def main():
     subparsers = parser.add_subparsers(dest="command")
 
     eval_parser = subparsers.add_parser("evaluate", help="Run evaluations")
-    eval_parser.set_defaults(func=run_evaluation)
+    eval_parser.set_defaults(func=benchci.evaluation.run_evaluation)
 
     page_parser = subparsers.add_parser("build-pages", help="Build HTML pages")
-    page_parser.set_defaults(func=build_pages)
+    page_parser.set_defaults(func=benchci.pages.build_pages)
 
     chart_parser = subparsers.add_parser("build-charts", help="Build charts")
-    chart_parser.set_defaults(func=build_charts)
+    chart_parser.set_defaults(func=benchci.charts.build_charts)
 
     compat_parser = subparsers.add_parser("compat", help="Compat logs")
-    compat_parser.set_defaults(func=compat_logs)
+    compat_parser.set_defaults(func=benchci.compat.compat_logs)
 
     server_parser = subparsers.add_parser("serve", help="Serve reports")
     server_parser.set_defaults(func=benchci.server.start_server)
