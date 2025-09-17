d180db3d2022c867eb847feb9b45d6b779a93e94
Chris <363708+christopherwoodall@users.noreply.github.com>
2025-09-17 13:57:59 -0500

    Update __init__.py

--- src/benchci/__init__.py
+++ src/benchci/__init__.py
@@ -3,11 +3,13 @@
 # Configure clean imports for the package
 # See: https://hynek.me/articles/testing-packaging/
 
-from . import compat, charts, evaluation, pages
+from . import compat, charts, evaluation, pages, serve
 from .compat import compat_logs
 from .charts import build_charts
 from .evaluation import load_config, run_evaluation
 from .pages import build_pages
+from .serve import serve_reports
+
 
 __all__ = [
     "compat",
@@ -22,4 +24,7 @@ __all__ = [
     
     "pages",
         "build_pages",
+
+    "serve",
+        "serve_reports",
 ]
